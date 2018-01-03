a22296a8aa44347680018babc84ab4ff50f08003
Rene Dohmen <rene@formatics.nl>
2018-01-03 20:31:45 +0100

    auth model

--- improviser/app.py
+++ improviser/app.py
@@ -3,16 +3,19 @@ import uuid
 
 import os
 import sys
-from flask import Flask, flash, request
+from flask import Flask, flash, request, url_for
 from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
+from flask_login import UserMixin, current_user
 from flask_migrate import Migrate, MigrateCommand
 from flask_restplus import Api, Resource, fields, marshal_with
 from flask_script import Manager
+from flask_security import RoleMixin, SQLAlchemyUserDatastore, Security, utils
 from flask_sqlalchemy import SQLAlchemy
+from markupsafe import Markup
 from sqlalchemy.dialects.postgresql.base import UUID
-
+from wtforms import PasswordField
 
 sys.path.append('../')
 from improviser.render.render import Render  # noqa
@@ -24,6 +27,17 @@ app.secret_key = 'TODO:MOVE_TO_BLUEPRINT'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://improviser:improviser@localhost/improviser'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECURITY_PASSWORD_HASH'] = 'pbkdf2_sha512'
+app.config['SECURITY_PASSWORD_SALT'] = 'SALTSALTSALT'
+
+# Replace the next six lines with your own SMTP server settings
+app.config['SECURITY_EMAIL_SENDER'] = os.getenv('SECURITY_EMAIL_SENDER') if os.getenv('SECURITY_EMAIL_SENDER') \
+    else 'no-reply@example.com'
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER') if os.getenv('MAIL_SERVER') else 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') if os.getenv('MAIL_USERNAME') else 'no-reply@example.com'
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') if os.getenv('MAIL_PASSWORD') else 'somepassword'
 
 # setup DB
 db = SQLAlchemy(app)
@@ -44,6 +58,7 @@ def version():
 
 riff_fields = {
     'id': fields.String,
+    'difficulty': fields.String,
     'name': fields.String,
     'number_of_bars': fields.Integer,
     'notes': fields.String,
@@ -51,7 +66,6 @@ riff_fields = {
     'image': fields.String,
     'render_valid': fields.Boolean,
     'render_date': fields.DateTime,
-
 }
 
 
@@ -68,6 +82,47 @@ def render(riff):
             print(f"Error: couldn't render riff.id: {riff.id}")
 
 
+# Define models
+class RolesUsers(db.Model):
+    __tablename__ = 'roles_users'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column('user_id', db.UUID(as_uuid=True), db.ForeignKey('user.id'))
+    role_id = db.Column('role_id', db.UUID(as_uuid=True), db.ForeignKey('role.id'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    # __str__ is required by Flask-Admin, so we can have human-readable values for the Role when editing a User.
+    def __str__(self):
+        return self.name
+
+    # __hash__ is required to avoid the exception TypeError: unhashable type: 'Role' when saving a User
+    def __hash__(self):
+        return hash(self.name)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    email = db.Column(db.String(255), unique=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary='roles_users',
+                            backref=db.backref('users', lazy='dynamic'))
+
+    # Human-readable values for the User when editing user related stuff.
+    def __str__(self):
+        return f'{self.username} : {self.email}'
+
+    # __hash__ is required to avoid the exception TypeError: unhashable type: 'Role' when saving a User
+    def __hash__(self):
+        return hash(self.email)
+
+
 class Riff(db.Model):
     __tablename__ = 'riffs'
     id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
@@ -81,6 +136,26 @@ class Riff(db.Model):
     def __repr__(self):
         return '<Riff %r>' % self.name
 
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# Executes before the first request is processed.
+@app.before_first_request
+def before_first_request():
+    user_datastore.find_or_create_role(name='admin', description='God mode!')
+    user_datastore.find_or_create_role(name='moderator', description='Can moderate other users content')
+    user_datastore.find_or_create_role(name='operator', description='Can create and block users')
+
+    # Create a default user
+    encrypted_password = utils.hash_password('acidjunk@gmail.com')
+    if not user_datastore.get_user('acidjunk@gmail.com'):
+        user_datastore.create_user(email='acidjunk@gmail.com', password=encrypted_password)
+        db.session.commit()
+        user_datastore.add_role_to_user('acidjunk@gmail.com', 'admin')
+        db.session.commit()
 
 @api.route('/api/riffs')
 class RiffListResource(Resource):
@@ -101,6 +176,9 @@ class RiffListResource(Resource):
 class PopulateAppResource(Resource):
 
     def get(self):
+        #Todo: fix chord + difficulty
+
+
         # Some fun stuff popular
         lily = []
         lily.append(("fis''8 e''16 b'8 g' fis''. e''16 b'8 g'8.", "medium", 1, "Careless whisper 1"))
@@ -147,16 +225,6 @@ class PopulateAppResource(Resource):
         lily.append(("c'8 f' bes' g' c'' f'' d'' g'' c''' d''' a'' e'' d'' g'' f'' c''8", "hard", 2,
                      "fourths in key hard"))
 
-
-
-
-
-
-
-
-
-
-
         for li in lily:
             riff = Riff(name=li[3], number_of_bars=li[2], notes=li[0], chord=li[1])
             try:
@@ -182,10 +250,58 @@ class RenderRiff(Resource):
             db.session.commit()
 
 
+class UserAdminView(ModelView):
+    # Don't display the password on the list of Users
+    column_exclude_list = list = ('password',)
+
+    # Don't include the standard password field when creating or editing a User (but see below)
+    form_excluded_columns = ('password',)
+
+    # Automatically display human-readable names for the current and available Roles when creating or editing a User
+    column_auto_select_related = True
+
+    # Prevent administration of Users unless the currently logged-in user has the "admin" role
+    def is_accessible(self):
+        #todo only admins allowed
+        return current_user.is_authenticated
+
+    # On the form for creating or editing a User, don't display a field corresponding to the model's password field.
+    # There are two reasons for this. First, we want to encrypt the password before storing in the database. Second,
+    # we want to use a password field (with the input masked) rather than a regular text field.
+    def scaffold_form(self):
+        # Start with the standard form as provided by Flask-Admin. We've already told Flask-Admin to exclude the
+        # password field from this form.
+        form_class = super(UserAdminView, self).scaffold_form()
+
+        # Add a password field, naming it "password2" and labeling it "New Password".
+        form_class.password2 = PasswordField('New Password')
+        return form_class
+
+    # This callback executes when the user saves changes to a newly-created or edited User -- before the changes are
+    # committed to the database.
+    def on_model_change(self, form, model, is_created):
+        # If the password field isn't blank...
+        if len(model.password2):
+            # ... then encrypt the new password prior to storing it in the database. If the password field is blank,
+            # the existing password in the database will be retained.
+            model.password = utils.hash_password(model.password2)
+
+class RolesAdminView(ModelView):
+
+    # Prevent administration of Roles unless the currently logged-in user has the "admin" role
+    def is_accessible(self):
+        #todo only admins and operators allowed
+        return current_user.is_authenticated
+
+
 class RiffAdminView(ModelView):
     column_default_sort = ('name', True)
     column_filters = ('number_of_bars', 'chord')
     column_searchable_list = ('name', 'chord')
+
+    def is_accessible(self):
+        #todo only admin, moderators and operators allowed
+        return current_user.is_authenticated
 
     @action('render', 'Render', 'Are you sure you want to re-render selected riffs?')
     def action_approve(self, ids):
@@ -202,8 +318,19 @@ class RiffAdminView(ModelView):
             if not self.handle_view_exception(error):
                 flash('Failed to re-render riff. {error}'.format(error=str(error)))
 
+    def _list_thumbnail(view, context, model, name):
+        print(context)
+        print(model)
+        return Markup('<img src="%s">' % url_for('static', filename=f'rendered/small/riff_{model.id}_c.png'))
+
+    column_formatters = {
+        'image': _list_thumbnail
+    }
+
 
 admin.add_view(RiffAdminView(Riff, db.session))
+admin.add_view(UserAdminView(User, db.session))
+admin.add_view(RolesAdminView(Role, db.session))
 
 if __name__ == '__main__':
     manager.run()
