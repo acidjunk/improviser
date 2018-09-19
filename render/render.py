aa6baca5d93c6315743eff3b428576c63a217663
www-data <www-data@blade3.colo.transip.net>
2018-09-19 08:32:11 +0200

    small fixes to renderer

--- render/render.py
+++ render/render.py
@@ -20,8 +20,7 @@
 # This script will produce a set of scales based on one predefined scale in all keys
 import os
 
-#SIZES = [80, 100, 120, 140, 160, 180, 200, 220]
-SIZES = [60]
+SIZES = [60, 80, 100, 120, 140, 160, 180, 200, 220]
 
 
 class Render:
