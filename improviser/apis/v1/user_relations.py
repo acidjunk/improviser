205433dd35f5129986961fe73eab12d6321be91b
oggiV <491216@student.fontys.nl>
2023-11-14 17:45:17 +0100

    Relation Endpoint Updates

--- improviser/apis/v1/user_relations.py
+++ improviser/apis/v1/user_relations.py
@@ -28,6 +28,28 @@ user_relation_serializer = api.model(
     },
 )
 
+user_model_fields = {
+    "id": fields.String(),
+    "first_name": fields.String(),
+    "last_name": fields.String()
+}
+
+school_model_fields = {
+    "id": fields.String(),
+    "name": fields.String()
+}
+
+user_relation_serializer_with_users = api.model(
+    "User Relation with Users",
+    {
+        "id": fields.String(),
+        "school": fields.Nested(school_model_fields),
+        "owner": fields.Nested(user_model_fields),
+        "teacher": fields.Nested(user_model_fields),
+        "student": fields.Nested(user_model_fields),
+    },
+)
+
 parser = api.parser()
 parser.add_argument("range", location="args", help="Pagination: default=[0,19]")
 parser.add_argument("sort", location="args", help='Sort: default=["name","ASC"]')
@@ -37,7 +59,7 @@ parser.add_argument("filter", location="args", help="Filter default=[]")
 @api.route("/")
 @api.doc("Create user relations")
 class SchoolsResourceList(Resource):
-    @marshal_with(user_relation_serializer)
+    @marshal_with(user_relation_serializer_with_users)
     @api.doc(parser=parser)
     def get(self):
         """List User Relations"""
@@ -47,7 +69,39 @@ class SchoolsResourceList(Resource):
         filter = get_filter_from_args(args)
 
         query_result, content_range = query_with_filters(UserRelation, UserRelation.query, range, sort, filter)
-        return query_result, 200, {"Content-Range": content_range}
+
+        serialized_result = []
+        for relation in query_result:
+            serialized_relation = {
+                "id": relation.id,
+                "school": {
+                    "id": relation.school.id,
+                    "name": relation.school.name,
+                },
+                "owner": {
+                    "id": relation.owner.id,
+                    "first_name": relation.owner.first_name,
+                    "last_name": relation.owner.last_name,
+                },
+            }
+
+            if relation.teacher:
+                serialized_relation["teacher"] = {
+                    "id": relation.teacher.id,
+                    "first_name": relation.teacher.first_name,
+                    "last_name": relation.teacher.last_name,
+                }
+
+            if relation.student:
+                serialized_relation["student"] = {
+                    "id": relation.student.id,
+                    "first_name": relation.student.first_name,
+                    "last_name": relation.student.last_name,
+                }
+
+            serialized_result.append(serialized_relation)
+
+        return serialized_result, 200, {"Content-Range": content_range}
 
     @roles_accepted("admin")
     @api.expect(user_relation_serializer)
@@ -55,12 +109,18 @@ class SchoolsResourceList(Resource):
     def post(self):
         owner = User.query.filter(User.id == api.payload["owner_id"]).first()
         teacher = User.query.filter(User.id == api.payload["teacher_id"]).first()
-        student = User.query.filter(User.id == api.payload["student_id"]).first()
 
-        if not owner or not teacher or not student:
-            abort(400, "User(s) not found")
+        if not owner or not teacher:
+            abort(400, "Owner or teacher not found")
 
-        relation = UserRelation(id=str(uuid.uuid4()), **api.payload, created_by=str(current_user.id))
+        student_id = api.payload.get('student_id')
+        if student_id:
+            student = User.query.filter(User.id == student_id).first()
+
+            if not student:
+                abort(400, "Student not found")
+
+        relation = UserRelation(id=str(uuid.uuid4()), **api.payload)
 
         save(relation)
         return relation, 201
@@ -70,20 +130,69 @@ class SchoolsResourceList(Resource):
 @api.doc("User Relation detail operations.")
 class RiffsToTagsResource(Resource):
     @roles_accepted("admin")
-    @marshal_with(user_relation_serializer)
+    @marshal_with(user_relation_serializer_with_users)
     def get(self, id):
         """List User Relation"""
         item = load(UserRelation, id)
-        return item, 200
 
+        serialized_relation = {
+            "id": item.id,
+            "school": {
+                "id": item.school.id,
+                "name": item.school.name,
+            },
+            "owner": {
+                "id": item.owner.id,
+                "first_name": item.owner.first_name,
+                "last_name": item.owner.last_name,
+            },
+        }
+
+        if item.teacher:
+            serialized_relation["teacher"] = {
+                "id": item.teacher.id,
+                "first_name": item.teacher.first_name,
+                "last_name": item.teacher.last_name,
+            }
+
+        if item.student:
+            serialized_relation["student"] = {
+                "id": item.student.id,
+                "first_name": item.student.first_name,
+                "last_name": item.student.last_name,
+            }
+
+        return serialized_relation, 200
+
+
+@api.route("/teacher/<id>")
+@api.doc("User Relation detail operations.")
+class RiffsToTagsResource(Resource):
     @roles_accepted("admin")
     @api.expect(user_relation_serializer)
     @api.marshal_with(user_relation_serializer)
-    def put(self, id):
+    def delete(self, id):
         """Edit User Relation"""
-        item = load(UserRelation, id)
-        item = update(item, api.payload)
-        return item, 201
+        all_items = UserRelation.query.all()
+
+        print(id)
+
+        payload = {
+            "teacher_id": None
+        }
+
+        for item in all_items:
+            print(item.teacher_id)
+            print(str(item.teacher_id) == id)
+            if str(item.teacher_id) == id:
+                update(item, payload)
+
+        return "", 204
+
+
+@api.route("/student/<id>")
+@api.doc("User Relation detail operations.")
+class RiffsToTagsResource(Resource):
 
     @roles_accepted("admin")
     def delete(self, id):
